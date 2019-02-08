a4bbd3bf46b8ba90b82df73f4b5d2c68ff9271a5
Arthur Guiot <arguiot@gmail.com>
2019-02-08 23:04:37 +0800

    Created KewordExtraction algorithm

---
+++ KeywordsExtraction.playground/Contents.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+let url = URL(string: "https://raw.githubusercontent.com/stopwords-iso/stopwords-iso/master/stopwords-iso.json")!
+let text = "How to eat pizza using only chopsticks"
+
+func extract(text: String, stop: [String: Any], lang: String = "en") -> [String] {
+    let stops = stop[lang] as! [String]
+    let tokens = text.split(separator: " ")
+    var out = [String]()
+    tokens.forEach { (substr) in
+        if !stops.contains(String(substr).lowercased()) {
+            out.append(String(substr))
+        }
+    }
+    return out
+}
+
+URLSession.shared.dataTask(with: url) { (data, response, error) in
+    if error != nil {
+        print(error!.localizedDescription)
+    }
+    
+    guard let data = data else { return }
+    //Implement JSON decoding and parsing
+    do {
+        let json = try JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+        let words = extract(text: text, stop: json)
+        print(words[0])
+    } catch let jsonError {
+        print(jsonError)
+    }
+    
+    
+}.resume()
+
+
